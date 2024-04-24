b654f33e5a34b120b4df290d2d8ab62a3d2a504e
Angel Ricardo Miss Blanco <angelitomiss@yahoo.com.mx>
2024-04-23 23:43:49 -0600

    third commit

--- chatbot.php
+++ chatbot.php
@@ -5,7 +5,16 @@
   chat-title="COINTESA"
   agent-id="3210bba5-1786-4647-8368-ab0dfb8a5f3e"
   chat-icon="assets/img/COINTESA1.png"
-  language-code="es"></df-messenger>
+  language-code="es">
+</df-messenger>
+
+<div class="container-boton">
+  <a href="https://wa.me/9212129413?text=Quiero%20realizar%20un%20proyecto%20web%20php" target="_blank">
+    <img class="boton" data-src="assets/img/whatsapp.png" src="assets/img/whatsapp.png" >
+  </a>
+</div>
+
+
 <style>
   df-messenger {
    --df-messenger-bot-message: #0051b3;
@@ -15,4 +24,23 @@
    --df-messenger-send-icon: #878fac;
    --df-messenger-user-message: #0051b3;
    }
+   .container-boton {
+    position: fixed;
+    bottom: 20px;
+    left: 30px; /* Cambia 'right' por 'left' para posicionarlo a la izquierda */
+    width: 40px;
+    height: 40px;
+    background-color: #0df053;
+    border-radius: 50%;
+    overflow: hidden; /* Añade esta propiedad para asegurarte de que el logotipo de WhatsApp no se salga del contenedor */
+    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.3);
+  }
+
+  .boton {
+      width: 100%;
+      height: auto;
+      max-width: 100%;
+      max-height: 100%;
+      transition: ease 1s;
+  }
 </style>
